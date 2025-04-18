c182fbf6037ab631144c7a3111d06440f1c01b8f
checksum-ai <114566276+checksum-ai[bot]@users.noreply.github.com>
2025-04-18 12:08:09 -0700

    updates-login-function

--- admin-frontend/checksum/login.ts
+++ admin-frontend/checksum/login.ts
@@ -1,33 +1,24 @@
-import { ChecksumConfig, IChecksumPage } from "@checksum-ai/runtime"
-import { expect, request } from "@playwright/test"
+import { ChecksumLoginFunction } from "@checksum-ai/runtime"
 
-export default async function login(
-  page: IChecksumPage,
-  config: ChecksumConfig
-) {
-  const apiContext = await request.newContext({
-    baseURL: "http://localhost:9000",
-  })
-  // const response = await apiContext.get("/reset-db")
-  // if (response.status() === 200) {
-  //   console.log("db resetted. Waiting for server to reload")
-  // } else {
-  //   console.log(response.status())
-  // }
-  // while (true) {
-  //   try {
-  //     const apiTestRequest = await apiContext.get("/")
-  //     await page.waitForTimeout(2000)
-  //     console.log("server responded. Proceeding to login")
-  //     break
-  //   } catch (error) {
-  //     console.log("backend not ready... trying in two seconds")
-  //     await page.waitForTimeout(2000)
-  //   }
-  // }
+const login: ChecksumLoginFunction = async (
+  page,
+  { user, environment, config }
+) => {
+  const username = user.username
+  const password = user.password
+
+  if (!username || !password) {
+    throw new Error("USERNAME and PASSWORD must be set")
+  }
+
   await page.goto("/login")
-  await page.getByPlaceholder("Email...").fill(config.username || "")
-  await page.getByPlaceholder("Password...").fill(config.password || "")
+  await page.getByPlaceholder("Email...").fill(username)
+  await page.getByPlaceholder("Password...").fill(password)
   await page.getByText("Continue").click()
   await page.waitForTimeout(3000)
+
+  console.log("Login Successful");
+  return
 }
+
+export default login
